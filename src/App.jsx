c4f0c072593db630d43b5ed6be3d6f4ed459e77a
abadanaf <abadanafchala@gmail.com>
2024-12-20 00:45:29 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Products from './components/Products';
-import Testimonials from './components/Testimonials';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Navbar from './components/Navbar/Navbar';
 
-function App() {
-    return (
-        <div>
-            <Navbar />
-            <Hero />
-            <Products />
-            <Testimonials />
-            <About />
-            <Contact />
-            <Footer />
-        </div>
-    );
+const App = () => {
+  return (
+    <div>
+      
+
+      <Navbar/>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/DarkMode.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import mode from "../../assets/mode.png"
+const DarkMode = () => {
+  return (
+    <div>
+      <img src={mode} alt="mode" />
+    </div>
+  );
+}
+
+export default DarkMode;
