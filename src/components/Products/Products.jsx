1053a05cd3c6f35fdd1e19040541d959502be2de
abadanaf <abadanafchala@gmail.com>
2024-12-23 19:01:49 +0300

    hero section

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Products = () => {
+  return (
+    <div>
+      {/* HeaderSections */}
+      <div className=''>
+        <p>Top Selling Products</p>
+        <h1>Products</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nobis tempore, assumenda sed officiis inventore eveniet quisquam mollitia quis temporibus, doloribus natus. Aliquid natus ullam, delectus aperiam sint aut facere.</p>
+      </div>
+      {/* Body Sections */}
+    </div>
+  );
+}
+
+export default Products;
