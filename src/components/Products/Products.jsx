5285d95a4de4a9071b7b648f2bb055316294438e
abadanaf <abadanafchala@gmail.com>
2024-12-23 20:38:28 +0300

    products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,92 @@
 import React from 'react';
+import prod1 from '../../assets/prod1.png';
+import prod2 from '../../assets/prod2.jpg';
+import prod3 from '../../assets/prod3.jpg';
+import prod4 from '../../assets/prod4.png';
+import prod5 from '../../assets/prod5.jpg';
+import { FaStar } from 'react-icons/fa6';
+
+const ProductsData = [
+    {
+        id:1,
+        img: prod1,
+        title: "Women Ethnic",
+        rating: 5.0,
+        author: "white",
+        aosDelay: "0",
+    },
+    {
+        id:2,
+        img: prod2,
+        title: "Women Ethnic",
+        rating: 5.0,
+        author: "white",
+        aosDelay: "0",
+    },
+    {
+        id:3,
+        img: prod3,
+        title: "Women Ethnic",
+        rating: 5.0,
+        author: "white",
+        aosDelay: "0",
+    },
+    {
+        id:4,
+        img: prod4,
+        title: "Women Ethnic",
+        rating: 5.0,
+        author: "white",
+        aosDelay: "0",
+    },
+    {
+        id:5,
+        img: prod5,
+        title: "Women Ethnic",
+        rating: 5.0,
+        author: "white",
+        aosDelay: "0",
+    },
+]
 
 const Products = () => {
   return (
-    <div>
-      {/* HeaderSections */}
-      <div className=''>
-        <p>Top Selling Products</p>
-        <h1>Products</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nobis tempore, assumenda sed officiis inventore eveniet quisquam mollitia quis temporibus, doloribus natus. Aliquid natus ullam, delectus aperiam sint aut facere.</p>
-      </div>
-      {/* Body Sections */}
+    <div className='mt-14 mb-12'>
+        <div className='container'>
+        {/* HeaderSections */}
+        <div className='text-center mb-10 max-w-[600px] mx-auto'>
+            <p data-aos="fade-up" className='text-sm text-primary'>Top Selling Products</p>
+            <h1 data-aos="fade-up" className='text-3xl font-bold'>Products</h1>
+            <p data-aos="fade-up" className='text-xs text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nobis tempore, assumenda sed officiis inventore eveniet quisquam mollitia quis temporibus, doloribus natus. Aliquid natus ullam, delectus aperiam sint aut facere.</p>
+        </div>
+        {/* Body Sections */}
+         <div>
+                <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5'>
+                     {/* cardSection */}
+                {ProductsData.map((data) =>(
+                    <div 
+                    data-aos="fade-up"
+                    data-aos-delay={data.aosDelay}
+                    key={data.id}
+                    className='space-y-3'
+                    >
+                        <img src={data.img} 
+                        alt=""
+                        className='h-[220px] w-[150px] object-cover rounded-md' 
+                        />
+                        <div>
+                            <h3 className='font-semibold'>{data.title}</h3>
+                            <p className='text-sm text-gray-600'>{data.author}</p>
+                            <div className='flex items-center gap-1'>
+                                <FaStar className='text-yellow-400'/>
+                                <span>{data.rating}</span>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+                </div>
+          </div>
+        </div>
     </div>
   );
 }
