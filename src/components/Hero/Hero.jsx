c95d784cf4aa34759dbdd2d9eee2a275522e9e10
abadanaf <abadanafchala@gmail.com>
2024-12-24 18:52:39 +0300

    order section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -33,7 +33,7 @@ const ImageList = [
     },
 ]
 
-const Hero = () => {
+const Hero = ({handleOrderPopup}) => {
 
     var settings = {
         dots: false,
@@ -80,7 +80,8 @@ const Hero = () => {
                         data-aos-delay="300"
                         >
                             <button
-                            className='bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full'>
+                            className='bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full'
+                            onClick={handleOrderPopup}>
                                 Order Now
                                 </button>
                         </div>
