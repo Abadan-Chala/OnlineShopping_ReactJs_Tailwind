641c95b17745803407758c3d4afb25e0f9fb6901
abadanaf <abadanafchala@gmail.com>
2024-12-24 08:47:29 +0300

    topProducts

--- src/components/TopProducts/TopProducts.jsx
+++ src/components/TopProducts/TopProducts.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const TopProducts = () => {
   return (
     <div>
-      
+      <div className='container'>
+        {/* header Section */}
+        <div className='text-center mb-10 max-w-[600px] mx-auto'>
+            <p data-aos="fade-up" className='text-sm text-primary'>Top Selling Products</p>
+            <h1 data-aos="fade-up" className='text-3xl font-bold'>TopProducts</h1>
+            <p data-aos="fade-up" className='text-xs text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nobis tempore, assumenda sed officiis inventore eveniet quisquam mollitia quis temporibus, doloribus natus. Aliquid natus ullam, delectus aperiam sint aut facere.</p>
+        </div>
+        {/* body Section */}
+
+      </div>
     </div>
   );
 }
