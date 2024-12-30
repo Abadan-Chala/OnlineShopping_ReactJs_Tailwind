29566d337a49ed07d400c9e2e37ffdedf0fe7a73
abadanaf <abadanafchala@gmail.com>
2024-12-30 22:23:58 +0300

    womens wear

--- src/components/Pages/Womens.jsx
+++ src/components/Pages/Womens.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Top1 from '../../assets/prod5.jpg';
-import Top2 from '../../assets/prod5.jpg';
-import Top3 from '../../assets/prod5.jpg';
-import Top4 from '../../assets/prod5.jpg';
+import Top1 from '../../assets/women1.jpg';
+import Top2 from '../../assets/women2.jpg';
+import Top3 from '../../assets/women3.jpg';
+import Top4 from '../../assets/women4.jpg';
 import Top5 from '../../assets/prod5.jpg';
 import { FaStar } from 'react-icons/fa6';
 
@@ -12,45 +12,45 @@ const ProductsData = [
   {
     id:1,
     img: Top1,
-    title:"Casual Wear",
-    description: "dsejdhjhfadkjfkfra0ejkdn  jajejfdkjelavfe sleipaeai opaemjeifed"
+    title:"Females Wear",
+    description: "this is the top rated product in the market which is very comfortable and stylish to wear."
 
   },
   {
     id:2,
     img: Top2,
-    title:"Casual Wear",
-    description: "dsejdhjhfadkjfkfra0ejkdn  jajejfdkjelavfe sleipaeai opaemjeifed"
+    title:"wedding Wear",
+    description: "this is wedding wear is the top rated product in the market which is very comfortable and stylish to wear."
 
   },
   {
     id:3,
     img: Top3,
-    title:"Casual Wear",
-    description: "dsejdhjhfadkjfkfra0ejkdn  jajejfdkjelavfe sleipaeai opaemjeifed"
+    title:"new Wear",
+    description: "this is the top rated product in the market which is very comfortable and stylish to wear."
 
   },
   {
     id:4,
     img: Top4,
-    title:"Casual Wear",
-    description: "dsejdhjhfadkjfkfra0ejkdn  jajejfdkjelavfe sleipaeai opaemjeifed"
+    title:"hijab Wear",
+    description: "muslim wear is the top rated product in the market which is very comfortable and stylish to wear."
 
   },
-  {
-    id:5,
-    img: Top5,
-    title:"Casual Wear",
-    description: "dsejdhjhfadkjfkfra0ejkdn  jajejfdkjelavfe sleipaeai opaemjeifed"
+  // {
+  //   id:5,
+  //   img: Top5,
+  //   title:"Casual Wear",
+  //   description: "dsejdhjhfadkjfkfra0ejkdn  jajejfdkjelavfe sleipaeai opaemjeifed"
 
-  },
-  {
-    id:6,
-    img: Top5,
-    title:"Casual Wear",
-    description: "dsejdhjhfadkjfkfra0ejkdn  jajejfdkjelavfe sleipaeai opaemjeifed"
+  // },
+  // {
+  //   id:6,
+  //   img: Top5,
+  //   title:"Casual Wear",
+  //   description: "dsejdhjhfadkjfkfra0ejkdn  jajejfdkjelavfe sleipaeai opaemjeifed"
 
-  },
+  // },
 ]
 
 const Womens = ({handleOrderPopup}) => {
@@ -61,7 +61,7 @@ const Womens = ({handleOrderPopup}) => {
         <div className='text-center mt-28 mb-10'>
             <p data-aos="fade-up" className='text-sm text-primary'>Wear</p>
             <h1 data-aos="fade-up" className='text-3xl font-bold'>Womens Wear</h1>
-            <p data-aos="fade-up" className='text-xs text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nobis tempore, assumenda sed officiis inventore eveniet quisquam mollitia quis temporibus, doloribus natus. Aliquid natus ullam, delectus aperiam sint aut facere.</p>
+            <p data-aos="fade-up" className='text-xs text-gray-400'>This is best and most comfortable wear for women.</p>
         </div>
         {/* body Section */}
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 md:gap-5 place-items-center'>
